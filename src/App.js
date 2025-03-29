02e8d3465810c7f480ce57c19497a1ed8f1b471f
unknown <blackskiesdancer@gmail.com>
2025-03-29 12:32:04 +1100

    initialised css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Flashcardlist from "./Flashcardlist";
+import './app.css'
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
