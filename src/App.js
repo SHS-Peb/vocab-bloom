e2e2f29de40798bc3a4ad0a1f336f506468e240b
unknown <blackskiesdancer@gmail.com>
2025-03-30 19:18:55 +1100

    connected API & UseEffect initialised

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Flashcardlist from "./Flashcardlist";
 import './app.css'
+import axios from 'axios'
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  
+useEffect(() => {
+  axios
+    .get('https://opentdb.com/api.php?amount=10')
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = questionItem.correct_answer
+        const options = [...questionItem.incorrect_answers, answer]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: questionItem.question,
+          answer: questionItem.correct_answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+      console.log(res.data);
+    })
+    .catch(error => console.error("Error fetching data:", error));
+}, []);
+
   return (
     <Flashcardlist flashcards={flashcards}/>
   );
